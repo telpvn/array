9d1ccb65a40c271487129fdcfd5b70143b65a37b
telpvn <telpvn@mail.ru>
2024-04-26 20:19:50 +0400

    array

---
+++ array/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[5];
+        for (int i = 0; i < 5; i++) {
+            array[i] = i + 10;
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(array[i]);
+        }
+
+    }
+}
